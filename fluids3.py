3fea0df8bfd452331df240f845093481da9dea8c
kb6737 <kb6737@mun.ca>
2017-06-27 18:31:07 -0230

    Add files via upload

--- fluids3.py
+++ fluids3.py
@@ -16,7 +16,7 @@
 import math
 
 
-def run1(vin, lvl1, lvl2, d, u, l, k):
+def run1(vin, lvl1, lvl2, d, u, l, k,pt1):
 	lvlloss = list()
 	# lAmbda = 0.3164/math.pow(re, 1./4)
 	# laMbda = math.pow(2*math.log((2.51/(re*math.sqrt(lAmbda)))+ 0.27/(d/k)),-2)
@@ -34,45 +34,64 @@ def run1(vin, lvl1, lvl2, d, u, l, k):
 		lvlloss.append((lvl1[n]-lvl2[n])*0.001)
 		vdot.append(vin[n]*lhvms)
 		v.append((4*vdot[n])/(math.pi * d * d))
-		re.append(v[n] * d / u)
-		if (re[n] > 2320) & (re[n] < math.pow(10, 5)):
-			lAmbda.append(0.3164/math.pow(re[n], 1./4))
-			sigma.append((2*lvlloss[n]*g)/(v[n]*v[n])-(lAmbda[n]*l*d))
+		re.append((v[n] * d )/ u)
+		lAmbda.append(0.3164/math.pow(re[n], 1./4))
+		print
+		print lAmbda[n] *l/d
+		print
+		print (2*lvlloss[n]*g)/(math.pow(v[n], 2))
+		print
+		if (re[n] > 2320) & (re[n] < math.pow(10, 5)) & pt1:
+			sigma.append(((2*lvlloss[n]*g)/(v[n]*v[n]))-(lAmbda[n]*l/d))
 			thl.append((lAmbda[n]*l/d)*(v[n]*v[n]/(2*g)))
-		elif re[n] > math.pow(10, 5):
+		else:
 			lAmbda.append(math.pow(2 * math.log((2.51 / (re[n] * math.sqrt(lAmbda[n]))) + 0.27 / (d / k)), -2))
-			sigma.append((2*lvlloss[n]*g)/(v[n]*v[n])-(lAmbda[n]*l*d))
+			sigma.append((2*lvlloss[n]*g)/(v[n]*v[n])-(lAmbda[n]*l/d))
 			thl.append((lAmbda[n]*l/d)*(v[n]*v[n]/(2*g)))
 	print 'Loss level m '+str(lvlloss)
+	print
 	print 'Flow rate m^3/s '+str(vdot)
+	print
 	print 'Flow velocity m/s '+str(v)
+	print
 	print "Renold's number "+str(re)
+	print
 	print 'Coefficient of pipe friction '+str(lAmbda)
+	print
 	print 'Length of pipe m '+str(l)
+	print
 	print 'Pipe roughness m '+str(k)
+	print
 	print 'Kinematic viscosity m^2/s '+str(u)
+	print
 	print 'Inside diameter of pipe m '+str(d)
+	print
 	print 'Resistance coefficient '+str(sigma)
+	print
 	print 'theoretical loss '+str(thl)
 
+	
 cPID = 0.016
 cPOD = 0.018
 pVCPID = 0.017
 pCVPOD = 0.020
-# copper
+# PVC
 t1vin = [200, 400, 600, 800, 1000, 1200, 1400]
 t1lvl1 = [591, 590, 599, 610, 619, 627, 635]
 t1lvl2 = [576, 552, 529, 497, 458, 415, 363]
-# PVC
-t2lvl1 = [573, 576, 589, 598, 618, 633, 656]
-t2lvl2 = [558, 534, 503, 466, 421, 363, 399]
+# copper
+t2lvl1 = [575, 583, 591, 598, 613, 631, 640]
+t2lvl2 = [559, 536, 505, 467, 425, 383, 316]
 # knee
 t3lvl1 = [511, 509, 512, 507, 499, 493, 483]
 t3lvl2 = [505, 490, 474, 446, 412, 377, 324]
 # elbow
 t4lvl1 = [540, 540, 540, 560, 580, 590, 620]
 t4lvl2 = [510, 490, 460, 430, 380, 330, 260]
-u = 1.297
+u = 1.297*math.pow(10,-6)
 l = 1
 k = (0.001)*1/1000
-run1(t1vin, t1lvl1, t1lvl2, cPID, u, l, k)
+run1(t1vin, t2lvl1, t2lvl2, cPID, u, l, k, True)
+run1(t1vin, t1lvl1, t1lvl2, pVCPID, u, l, k, True)
+run1(t1vin, t3lvl1, t3lvl2, pCVPID, u, , k, False)
+run1(t1vin, t4lvl1, t4lvl2, pCVPID, u, , k, False)
